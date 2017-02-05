6baed4a6457624a6ecd54ec7be54dcd18b4801b3
Pat Jacobs <musicbender@gmail.com>
2017-02-05 15:04:57 -0800

    expanded reducer to check and change all block types when num keys are pressed

--- src/sub-apps/brew-calculator/reducers/reducer_values.js
+++ src/sub-apps/brew-calculator/reducers/reducer_values.js
@@ -11,11 +11,23 @@ export default function values(state = initialState, action) {
     case NUM_CHANGE:
       switch (action.block) {
         case "coffee":
-          return {...state, coffee: action.input}
+          if (!state.coffee) {
+            return {...state, coffee: action.input}
+          } else {
+            return {...state, coffee: `${state.coffee}${action.input}`}
+          }
         case "water":
-          return{...state, water: action.input}
+          if (!state.water) {
+            return {...state, water: action.input}
+          } else {
+            return {...state, water: `${state.water}${action.input}`}
+          }
         case "ratio":
-          return {...state, ratio: action.input}
+          if (!state.ratio) {
+            return {...state, ratio: action.input}
+          } else {
+            return {...state, ratio: `${state.ratio}${action.input}`}
+          }
         default:
           return state;
       }
