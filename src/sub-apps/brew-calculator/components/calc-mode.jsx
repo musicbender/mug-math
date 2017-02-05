bbdc5eca8c0cea687670ecc0605f13698b00b420
Pat Jacobs <musicbender@gmail.com>
2017-02-05 00:15:38 -0800

    preparing styling of brew calc top section

--- src/sub-apps/brew-calculator/components/calc-mode.jsx
+++ src/sub-apps/brew-calculator/components/calc-mode.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import '../style/calc-modes.scss';
 
 export default (props) => {
   const { prefix } = props;
@@ -6,13 +7,13 @@ export default (props) => {
   return (
     <div>
       <div className={`${prefix}-calc-container first`}>
-        <div>0g of Coffee</div>
+        <div className="calc-body">0g of Coffee</div>
       </div>
       <div className={`${prefix}-calc-container second`}>
-        <div>0g of Water</div>
+        <div className="calc-body">0g of Water</div>
       </div>
       <div className={`${prefix}-calc-container result`}>
-        <div>a Ratio of 0:0</div>
+        <div className="calc-body">a Ratio of 0:0</div>
       </div>
     </div>
   )
