424031fc75a67dc35bef05f5adfd25175c8d64e4
Pat Jacobs <musicbender@gmail.com>
2017-02-08 20:08:42 -0800

    created max number limit feature and encapsilated number functions into an object

--- src/sub-apps/brew-calculator/reducers/reducer_values.js
+++ src/sub-apps/brew-calculator/reducers/reducer_values.js
@@ -10,81 +10,87 @@ export default function values(state = initialState, action) {
   switch(action.type) {
     case NUM_CHANGE:
 
-      function onlyOneDecimal(current) {
-        return current.indexOf('.') > -1 ? false : true;
-      }
+      const newNumber = {
+        onlyOneDecimal: function(current) {
+          return current.indexOf('.') > -1 ? false : true;
+        },
 
-      function deleteLast(current) {
-        var newInput;
+        deleteLast: function(current) {
+          var newInput;
 
-        if (current.length === 1 || !current.length) {
-          newInput = 0;
-        } else {
-          let c = current.substr(0, current.length-1);
-          newInput = c;
+          if (current.length === 1 || !current.length) {
+            newInput = 0;
+          } else {
+            let c = current.substr(0, current.length-1);
+            newInput = c;
+          }
+          return newInput;
+        },
+
+        findMax: function(current) {
+          const decimal = current.indexOf(".");
+          var max;
+
+          switch(decimal) {
+            case -1:
+              max = 3;
+              break;
+            case 1:
+              max = 4;
+              break;
+            case 2:
+              max = 5;
+              break;
+            case 3:
+              max = 6;
+              break;
+          }
+
+          return max;
+        },
+
+        isNotMax: function(current) {
+          return current.length >= this.findMax(current) ? false : true;
+        },
+
+        output: function(input, current) {
+          var output;
+
+          switch (true) {
+            case (!current && input !== "delete" && input !== "." && input !== "0"):
+              output = input;
+              break;
+            case (!current && input === "."):
+              output = `0${input}`;
+              break;
+            case (!current && input === "0"):
+              output = current;
+              break;
+            case (input === "delete"):
+              output = this.deleteLast(current);
+              break;
+            case (input === "." && this.onlyOneDecimal(current)):
+              output = current + input;
+              break;
+            case (input === "." && !(this.onlyOneDecimal(current))):
+              console.error('too many decibles');
+              output = current;
+              break;
+            case (input !== "." && input !== "delete"):
+              output = this.isNotMax(current) ? `${current}${input}` : current;
+              break;
+            default:
+              console.error('output error to default');
+              output = current;
+          }
+
+          return output;
         }
-        return newInput;
       }
-
-      function numberMax(input, current) {
-        const decimal = current.search(".");
-        var max;
-
-        switch(decimal) {
-          case -1:
-            max = 3;
-            break;
-          case 1:
-            max = 4;
-            break;
-          case 2:
-            max = 5;
-            break;
-          case 3:
-            max = 6;
-            break;
-        }
-
-      }
-
-      function newNumber(input, current) {
-        var output;
-
-        switch (true) {
-          case (!current && input !== "delete" && input !== "." && input !== "0"):
-            output = input;
-            break;
-          case (!current && input === "."):
-            output = `0${input}`;
-            break;
-          case (!current && input === "0"):
-            output = current;
-            break;
-          case (input === "delete"):
-            output = deleteLast(current);
-            break;
-          case (input === "." && onlyOneDecimal(current)):
-            output = current + input;
-            break;
-          case (input === "." && !(onlyOneDecimal(current))):
-            console.error('too many decibles');
-            output = current;
-            break;
-          case (input !== "." && input !== "delete"):
-            output = `${current}${input}`;
-            break;
-          default:
-            console.error('output error to default');
-            output = current;
-        }
-
-        return output;
-      }
-
 
       switch (action.block) {
         case "coffee":
-          return {...state, coffee: newNumber(action.input, state.coffee)}
+          return {...state, coffee: newNumber.output(action.input, state.coffee)}
         case "water":
           break;
         case "ratio":
