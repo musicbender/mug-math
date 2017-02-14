eb51faf7ed63017186b77eab5c78963add85359c
Pat Jacobs <musicbender@gmail.com>
2017-02-13 22:47:22 -0800

    created array map for building blocks out dynamically and more dry

--- src/sub-apps/brew-calculator/components/block.jsx
+++ src/sub-apps/brew-calculator/components/block.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 export default (props) => {
-  const { mode, block, changeBlock, name, order} = props;
+  const { mode, block, changeBlock, name, unit, text, order} = props;
 
   const handleClick = () => {
     if (notResult()) {
@@ -10,16 +10,17 @@ export default (props) => {
   }
 
   const notResult = () => {
-    return order !== "result" ? true : false;
+    return order !== 2 ? true : false;
   }
 
   const selected = () => {
     return (name === block && notResult()) ? <span>--</span> : '';
   }
+  console.log(`${block}: ${order}`)
 
   return (
-    <div className={`brew-block-${mode} brew-block ${order} brew-${name}`} onClick={() => handleClick()}>
-      <div className="brew-calc-body">{selected()}{props.children}</div>
+    <div className={`brew-block-${mode} brew-block brew-${name}`} onClick={() => handleClick()}>
+      <div className="brew-calc-body">{selected()}</div>
     </div>
   )
 }
