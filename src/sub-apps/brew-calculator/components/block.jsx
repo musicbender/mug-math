a66352163e7c559772ab823f0b67f646ca56f923
Pat Jacobs <musicbender@gmail.com>
2017-02-20 18:20:47 -0800

    added plus minus buttons to brew boxes

--- src/sub-apps/brew-calculator/components/block.jsx
+++ src/sub-apps/brew-calculator/components/block.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
+import IconButton from 'material-ui/IconButton';
+import ContentAdd from 'material-ui/svg-icons/content/add'
+import ContentRemove from 'material-ui/svg-icons/content/remove'
+import iconStyles from '../style/icons';
 
 export default (props) => {
   const { mode, block, changeBlock, name, unit, text, order} = props;
+  const iconColor = 'rgba(0, 77, 64, 0.9)';
 
   const handleClick = () => {
     if (notResult()) {
@@ -25,6 +30,12 @@ export default (props) => {
           <div className="brew-value">
             {name === "ratio" ? `1:${props[name]}` : `${props[name]}`}<span className="brew-unit">{unit}</span>
           </div>
+          <IconButton style={iconStyles.plus} iconStyle={iconStyles.icon}>
+            <ContentAdd color={iconColor} />
+          </IconButton>
+          <IconButton style={iconStyles.minus} iconStyle={iconStyles.icon}>
+            <ContentRemove color={iconColor} />
+          </IconButton>
         </div>
       )
     } else {
---
+++ src/sub-apps/brew-calculator/style/icons.js
@@ -0,0 +1,20 @@
+const iconStyles = {
+  plus: {
+    position: 'absolute',
+    top: '15%',
+    left: '90%',
+    transform: 'translate(-50%,-50%)'
+  },
+  minus: {
+    position: 'absolute',
+    top: '85%',
+    left: '90%',
+    transform: 'translate(-50%,-50%)'
+  },
+  icon: {
+    fontSize: '1em',
+    color: 'rgba(0, 77, 64, 0.9)'
+  }
+}
+
+export default iconStyles;
