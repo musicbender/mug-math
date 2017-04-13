12abcb5bcefb4b4dbaf8cfa65fa0d76a3e829544
Pat Jacobs <musicbender@gmail.com>
2017-04-12 22:39:06 -0700

    fixed time picker number ppositioning

--- src/components/menu-item.jsx
+++ src/components/menu-item.jsx
@@ -1,23 +1,18 @@
 import React from 'react';
 import { Link } from 'react-router';
 
-const r = 90;
-
 export default (props) => {
-  const isActive = () => {
-    const thisPath = `sub-apps${props.url}/`;
+  const r = 90;
+  const thisPath = `sub-apps${props.url}/`;
 
+  const isActive = () => {
     return props.currentPath === thisPath ? 'active' : '';
   }
 
   return (
     <div className={`menu-item-container ${isActive()}`}>
-      <Link
-        to={`sub-apps${props.url}/`}
-        className="menu-circle"
-        onlyActiveOnIndex
-      >
-          {props.children}
+      <Link to={thisPath} className="menu-circle"onlyActiveOnIndex>
+        {props.children}
       </Link>
       <svg className="menu-text" version="1.1" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="300px" height="300px" viewBox="0 0 300 300">
           <defs>
--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -2,45 +2,14 @@ import React, { Component } from 'react';
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import { connect } from 'react-redux';
-import { black, grey900, grey700, grey500, grey100, white } from 'material-ui/styles/colors';
+import muithemeStyle from '../style/muitheme-style';
 import HomeMenu from './Home-Menu.jsx';
 
 var muiTheme;
 
 class App extends Component {
   componentWillMount() {
-    const initColor = 'rgb(40,40,40)';
-
-    muiTheme = getMuiTheme({
-      palette: {
-        primary1Color: black,
-        primary2Color: grey900,
-        primary3Color: grey500,
-        accent1Color: black,
-        accent2Color: grey100,
-        accent3Color: grey500,
-        pickerHeaderColor: grey700,
-      },
-      appBar: {
-        height: 45,
-        shadow: "none",
-        color: black
-      },
-      tabs: {
-        color: black
-      },
-      tab: {
-        color: black
-      },
-      slider: {
-        handleColorZero: 'transparent',
-        handleFillColor: initColor,
-        handleSize: 20,
-        selectionColor: initColor,
-        rippleColor: initColor
-      },
-      userAgent: 'all'
-    });
+    muiTheme = getMuiTheme(muithemeStyle);
   }
 
   render() {
