1098a086686edd5a2cf7dd6cb4c5f5a189cac657
musicbender <pat@patjacobs.io>
2017-09-17 17:21:57 -0700

    adjusted curved menu text

--- src/components/menu-item.jsx
+++ src/components/menu-item.jsx
@@ -2,7 +2,8 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const MenuItem = (props) => {
-  const r = 90;
+  const r = 65;
+  const viewBox = '50 50 125 125';
   const isPageLoaded = () => {
     return props.loaded ? 'not-loaded' : 'loaded';
   }
@@ -12,7 +13,7 @@ const MenuItem = (props) => {
       <Link to={props.path} className="menu-circle">
         {props.children}
       </Link>
-      <svg className="menu-text" version="1.1" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="125px" height="125px" viewBox="50 50 125 125">
+      <svg className="menu-text" version="1.1" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="125px" height="125px" viewBox={viewBox}>
         <defs>
           <path id="criclePath" d={`M 150, 150 m -${r}, 0 a ${r},${r} 0 0,1 ${r*2},0 a ${r},${r} 0 0,1 -${r*2},0`} />
         </defs>
