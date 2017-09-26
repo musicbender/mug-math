47d8ca957e1be2513cc840b1bfa36e6db540ff42
musicbender <pat@patjacobs.io>
2017-09-25 23:56:41 -0700

    added pug templating for serving html

--- server/server.js
+++ server/server.js
@@ -14,15 +14,16 @@ require('babel-core/register')({
     presets: ['es2015', 'react']
 });
 
+console.log(`live? ${process.env.LIVE}`);
+
 const PORT = process.env.PORT || 3001;
 const app = new express();
 
-console.log(process.env.PORT);
-console.log(`live? ${process.env.LIVE}`);
-console.log(`process: ${process.env.NODE_ENV}`);
-console.log(__dirname);
+app.set('view engine', 'pug');
+app.set('views', 'server/views');
 
 // app.use('/', httpsRedirect(true));
+
 app.use('/public', express.static(path.join(__dirname, '/static')));
 
 app.use('/', (req, res, next) => {
@@ -56,53 +57,8 @@ app.use('/', (req, res, next) => {
     res
       .set('Content-Type', 'text/html')
       .status(200)
-      .send(renderFullPage(html, preloadedState))
+      .render('index', {html, preloadedState});
 });
-
-const renderFullPage = (html, initialState) => {
-  return `
-    <!doctype html>
-    <html class="no-js" lang="">
-      <head>
-        <meta charset="UTF-8">
-        <link rel="manifest" href="manifest.json">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-        <meta name="mobile-web-app-capable" content="yes">
-        <meta name="apple-mobile-web-app-capable" content="yes">
-        <meta name="msapplication-starturl" content="/">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="theme-color" content="#000000"/>
-        <title>Mug Math</title>
-        <link href="https://fonts.googleapis.com/icon?family=Material+Icons"
-          rel="stylesheet" defer></link>
-        <link href="/public/style.css" rel="stylesheet">
-      </head>
-      <body>
-        <script>
-          window.__INITIAL_STATE__ = ${JSON.stringify(initialState)};
-        </script>
-
-        <!--[if lt IE 8]>
-            <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
-        <![endif]-->
-
-        <div id="app">${html}</div>
-
-        <!-- <noscript>
-          <div class="no-script" style="position: absolute; left: 50%; top: 33%; transform: translate(-50%,-50%); width: 40em; height: 10em; background-color: #fff; padding: 2em; font-size: 1.2em">
-            <p>You have turned off javascript or your browser does not support it. You'll have to enable it or use a different browser to us MugMath! :( </p>
-          </div>
-        </noscript> -->
-
-        <div></div>
-        <div></div>
-
-        <script type="text/javascript" src="/public/vendor.js"></script>
-        <script type="text/javascript" src="/public/dist.js"></script>
-      </body>
-    </html>
-  `;
-};
 
 if (!process.env.LIVE) {
   const options = {
