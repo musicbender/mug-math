0ba2208e13c6ec45d7fcf205af94437aee531355
Pat Jacobs <pjacobs@PJacobs-nb.local>
2017-09-28 23:52:52 -0700

    created deployment node files using node rsync

--- tools/deploy.js
+++ tools/deploy.js
@@ -1,6 +1,25 @@
 /* eslint-disable no-console */
+import Rsync from 'rsync';
 
 process.env.NODE_ENV = 'production';
 process.env.LIVE = 'true';
 
 console.log('Deploying to live server. This will take a moment...');
+
+const rsync = new Rsync()
+  .flags('avz')
+  .source('dist/')
+  .destination('patjacobs:/home/patjacob/mug-math/dist/')
+  .set('delete');
+
+  rsync.execute((error, code, cmd) => {
+      if (error) {
+        console.log(error.message);
+      }
+      console.log(`Rync of package.json a success!`);
+      console.log(cmd);
+  }, (data) => {
+    console.log(`OUT: ${data}`);
+  }, (data) => {
+    console.log(`ERR: ${data}`);
+  });
