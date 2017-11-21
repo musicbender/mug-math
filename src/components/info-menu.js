49ba57e55245dfffd16bf5439bc8097fae964e7d
musicbender <musicbender@gmail.com>
2017-11-20 23:53:11 -0800

    styled and animated info menu items

--- src/components/info-menu.js
+++ src/components/info-menu.js
@@ -3,15 +3,30 @@ import { Route, Link } from 'react-router-dom';
 import nav from '../constants/nav.json';
 
 export default (props) => {
+  const itemHeight = `${100 / nav.infoMenu.length}%`;
+
+  const itemStyles = {
+    height: itemHeight,
+    lineHeight: itemHeight,
+  };
+
   return (
      <div className="info-menu-container info-menu-item">
       {
         nav.infoMenu.map((item,index) => {
           switch (item.id) {
             case "about" :
-              return <Link to={item.url} key={Math.random()}>{item.title}</Link>;
+              return (
+                <div key={Math.random()} style={itemStyles}>
+                  <Link to={item.url}>{item.title}</Link>
+                </div>
+              );
             default:
-              return <a href={item.url} target="_blank" key={Math.random()}>{item.title}</a>
+              return (
+                <div key={Math.random()} style={itemStyles}>
+                  <a href={item.url} target="_blank">{item.title}</a>
+                </div>
+              );
           }
         })
       }
