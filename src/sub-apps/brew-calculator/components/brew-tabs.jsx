89aa946fde8314fdf0e307da3e8fe5df9bfe654c
Pat Jacobs <musicbender@gmail.com>
2018-04-21 22:39:28 -0700

    more styling enhancements

--- src/sub-apps/brew-calculator/components/brew-tabs.jsx
+++ src/sub-apps/brew-calculator/components/brew-tabs.jsx
@@ -7,7 +7,6 @@ export default (props) => {
 
   const tabStyle = {
     backgroundColor: grey900,
-    backgroundImage: `url("data:image/svg+xml,%3Csvg width='6' height='6' viewBox='0 0 6 6' xmlns='http://www.w3.org/2000/svg'%3E%3Cg fill='%23000000' fill-opacity='1' fill-rule='evenodd'%3E%3Cpath d='M5 0h1L0 6V5zM6 5v1H5z'/%3E%3C/g%3E%3C/svg%3E")`
   }
 
   const handleTabChange = (value) => {
